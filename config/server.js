98864cb3d1d33c21957383722784d7c6e6fdcac8
Matheus Moraes <matheus@xcoding.com.br>
2021-02-11 17:08:16 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e192b7d266b1b33e4a167e432c77891'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6257faa4-adcc-4d5d-bf9e-49f2e685ba12'
+};
