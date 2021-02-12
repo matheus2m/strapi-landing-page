fcf44de30526ed7f0304f93febc518fb1bbb7764
Matheus Moraes <matheusrobaskievicz@hotmail.com>
2021-02-11 22:56:57 -0300

    add sections review e faq; alterado config database

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+//dump command: docker exec -i postgres psql -h 127.0.0.1 -U postgres -d strapi -W < strapi.sql
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -5,11 +7,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '172.20.2.40'),
+        host: env('DATABASE_HOST', process.env.DATABASE_HOST || '172.20.2.40'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '1234'),
+        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD || '1234'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
